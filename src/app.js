d4e1eb7185aa19e12e007c120f840763a81017c7
D I <irsooti@gmail.com>
2018-09-05 15:55:47 +0200

    Errore umano

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const db = require('./database/instance').getInstance();
 const queries = require('./database/queries')(db);
 const docxGenerator = require('./service/docxGenerator');
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 module.exports = {
   start: () => {
@@ -15,14 +15,25 @@ module.exports = {
         queryResolve => {
           docxGenerator.generate(queryResolve.data, req.params.idutente).then(
             docxResolve => {
-              res.download(docxResolve.downloadLink, `dipendente-${req.params.idutente}.docx`)
+              res.download(
+                docxResolve.downloadLink,
+                `dipendente-${req.params.idutente}.docx`
+              );
             },
             docxErr => {
-              res.send("Si è verificato un errore durante la generazione del documento").status(500);
+              console.log('[ERRORE GENERAZIONE DOCUMENTO]', docxErr);
+              res
+                .send(
+                  'Si è verificato un errore durante la generazione del documento'
+                )
+                .status(500);
             }
           );
         },
-        queryErr => res.send("L\'utente non ha ancora aggiornato il CV").status(500)
+        queryErr => {
+          console.log('[ERRORE CV NON PRESENTE]', queryErr);
+          res.send("L'utente non ha ancora aggiornato il CV").status(500);
+        }
       );
     });
 
