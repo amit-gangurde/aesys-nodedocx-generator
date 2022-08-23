a2234065c096c24ae2e821971449dce1d4063ece
Amit Gangurde <amit.gangurde@enzigma.in>
2022-08-23 14:52:18 +0530

    commit code

--- src/app.js
+++ src/app.js
@@ -11,20 +11,20 @@ module.exports = {
     app.get('/docGenerator', (req, res) => {
       
       const userId = '1';
-         docxGenerator.generate({"name": "AjaySingh Chavan" ,"profile":"ORD-19082022","company":"Enzigma Software Private Limited","product":[
-          {"sr":"1", "name":"Pro-Fit","quantity":"1","price":"232"},
-          {"sr":"2", "name":"Pro-Fit","quantity":"1","price":"554"},
-          {"sr":"3", "name":"Pro-Fit","quantity":"1","price":"278"},
-          {"sr":"4", "name":"Pro-Fit","quantity":"1","price":"921"}]}, userId).then(
+         docxGenerator.generate({"name": "Amit Gangurde" ,"orderid":"ORD-19082022","company":"Enzigma Software Private Limited","product":[
+          {"sr":"1", "name":"Laptop","quantity":"1","price":"22"},
+          {"sr":"2", "name":"Keyboard","quantity":"10","price":"554"},
+          {"sr":"3", "name":"Mouse","quantity":"3","price":"278"},
+          {"sr":"4", "name":"Monitor","quantity":"2","price":"921"}]}, userId).then(
             docxResolve => {
-              console.log(`[GENERATED DOCUMENT][#${userId}]`);
+              console.log(`[Generated Document][#${userId}]`);
               res.download(
                 docxResolve.downloadLink,
                 `OrderEmail-${userId}.docx`
               );
             },
             docxErr => {
-              console.log('[DOCUMENT GENERATION ERROR]', docxErr);
+              console.log('[Document Generated Error]', docxErr);
               res
                 .send(
                   'An error occurred while generating the document'
--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
 const dotenv = require('dotenv').load();
 const App = require('./app');
-
-App.
-start();
+App.start();
